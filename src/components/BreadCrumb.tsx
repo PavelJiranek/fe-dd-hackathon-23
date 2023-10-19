e1e6e51e4b3c87cb609d1ebd8791288da7ca3447
davidzoufaly <davidzoufaly@gmail.com>
2023-10-19 21:26:11 +0200

    feat: breadcrumb added to 1 level

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -8,7 +8,12 @@ export const BreadCrumb = () => {
     const pathname = window.location.pathname;
     const breadCrumbLevels = pathnameToArray(pathname);
     return (
-        <Breadcrumb spacing="8px" separator={<ChevronRightIcon color="gray.500" />}>
+        <Breadcrumb
+            spacing="8px"
+            separator={<ChevronRightIcon color="gray.500" />}
+            paddingLeft={"2rem"}
+            paddingTop={"2rem"}
+        >
             {breadCrumbLevels?.map(({ original, modified }) => (
                 <BreadcrumbItem key={modified} fontSize={"18px"} color={"#2D3748"} fontWeight={"600"}>
                     <BreadcrumbLink href={substringToEnd(pathname, original)}>{modified}</BreadcrumbLink>
--- src/pages/Workspaces.tsx
+++ src/pages/Workspaces.tsx
@@ -3,11 +3,13 @@ import React, { FC } from "react";
 
 import { Layout } from "../components/Layout.js";
 import { WorkspacesList } from "../components/WorkspacesList/WorkspacesList.js";
+import { BreadCrumb } from "../components/BreadCrumb.js";
 
 export const Workspaces: FC = () => {
     return (
         <Layout>
             <Stack>
+                <BreadCrumb />
                 <WorkspacesList />
             </Stack>
         </Layout>
