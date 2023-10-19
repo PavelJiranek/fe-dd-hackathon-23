c4ce6f4c51b9e3f04acad48b52af002a92a32731
davidzoufaly <davidzoufaly@gmail.com>
2023-10-19 14:53:20 +0200

    feat: login form styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
 // (C) 2023 GoodData Corporation
 
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import { LoginPage } from "./pages/LoginPage.js";
+import React from "react";
 import { useBackendStore } from "./store/useBackendStore.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppRoutes } from "./constants/constants.js";
 import { PrivateRoute } from "./components/PrivateRoute.js";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export const App: React.FC = () => {
     const store = useBackendStore();
 
+    const {} = store;
     console.log(store);
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    path={AppRoutes.Home}
-                    element={
-                        <PrivateRoute>
-                            <>Home</>
-                        </PrivateRoute>
-                    }
-                />
-                <Route path={AppRoutes.Login} Component={LoginPage} />
-            </Routes>
-        </BrowserRouter>
+        <ChakraProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path={AppRoutes.Home}
+                        element={
+                            <PrivateRoute>
+                                <>Home</>
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route path={AppRoutes.Login} Component={LoginPage} />
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
     );
 };
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,22 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, Box } from "@chakra-ui/react";
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+    Box,
+    Card,
+    CardBody,
+    Text,
+    Center,
+    Button,
+    CardFooter,
+    CardHeader,
+    Heading,
+    Stack,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 import { useBackendStore } from "../store/useBackendStore.js";
@@ -29,27 +45,61 @@ export const LoginPage: React.FC = () => {
         navigate(AppRoutes.Home);
     };
 
-    console.log(errors);
-
     return (
         <Box w="100%" h="100%" backgroundColor="#FFF5F7">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <FormControl isInvalid={!!errors.domain}>
-                    <FormLabel>Domain</FormLabel>
-                    <Input {...register("domain", { required: true })} />
-                    {!errors.domain && <FormHelperText>Please enter your GoodData domain</FormHelperText>}
-                    <FormLabel>API Token</FormLabel>
-                    <Input {...register("token", { required: true })} />
-                    {errors.domain?.type === "required" ? (
-                        <FormErrorMessage>This is required</FormErrorMessage>
-                    ) : (
-                        <FormErrorMessage>{errors?.domain?.message}</FormErrorMessage>
-                    )}
-                    {!errors && <FormHelperText>Please enter your API Token</FormHelperText>}
-                    {/* TODO: Validation of the Token */}
-                    <input type="submit" />
-                </FormControl>
-            </form>
+            <Center minH="100vh">
+                <Card backgroundColor={"white"} borderRadius={"8px"} width={"30rem"}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <FormControl isInvalid={!!errors.domain}>
+                            <CardHeader>
+                                <Heading size="md">Organization management</Heading>
+                            </CardHeader>
+                            <CardBody>
+                                <Stack paddingBottom={"1rem"}>
+                                    <Text>
+                                        <FormLabel>Domain</FormLabel>
+                                    </Text>
+                                    <Input
+                                        {...register("domain", { required: true })}
+                                        placeholder={"xyz.trial.cloud.gooddata.com"}
+                                    />
+                                    {!errors.domain && (
+                                        <FormHelperText>Please enter your GoodData domain</FormHelperText>
+                                    )}
+                                    {errors.domain?.type === "required" ? (
+                                        <FormErrorMessage>This is required</FormErrorMessage>
+                                    ) : (
+                                        <FormErrorMessage>{errors?.token?.message}</FormErrorMessage>
+                                    )}
+                                </Stack>
+                                <Stack>
+                                    <Text>
+                                        <FormLabel>API Token</FormLabel>
+                                    </Text>
+                                    <Input
+                                        {...register("token", { required: true })}
+                                        placeholder={"API token"}
+                                    />
+                                    {errors.domain?.type === "required" ? (
+                                        <FormErrorMessage>This is required</FormErrorMessage>
+                                    ) : (
+                                        <FormErrorMessage>{errors?.domain?.message}</FormErrorMessage>
+                                    )}
+                                    {!errors.token && (
+                                        <FormHelperText>Please enter your API Token</FormHelperText>
+                                    )}
+                                    {/* TODO: Validation of the Token */}
+                                </Stack>
+                            </CardBody>
+                            <CardFooter justifyContent="flex-end">
+                                <Button type="submit" size="md" variant="solid" colorScheme="blue">
+                                    Continue
+                                </Button>
+                            </CardFooter>
+                        </FormControl>
+                    </form>
+                </Card>
+            </Center>
         </Box>
     );
 };
